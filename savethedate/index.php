de51fd681a3883edcf76fc4529a0afdbf4497cdb
chilootz <charlie@nazarios.net>
2016-01-11 21:22:59 -0500

    initial commit of save the date subsite

---
+++ savethedate/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <title>Save the Date | East Coast Boy Meets West Coast Girl</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="top row"></div>
+        <div class="middle row">
+            <h1>Save the Date</h1>
+            <p>August 27, 2016</p>
+            <p>Seattle, Washington</p>
+        </div>
+        <div class="bottom row">
+            <div class="col-4"><img src="images/klos-morocco.jpg"/></div>
+            <div class="col-4"><img src="images/klos-video-frame-button.jpg"/></div>
+            <div class="col-4"><img src="images/klos-garden.jpg"></div>
+        </div>
+    </div>
+</body>
+</html>
